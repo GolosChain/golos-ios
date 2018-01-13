0d89caa5b7faa0a1aad07f5110a7172e64afb2e2
Grigory Serebryanyy <grishamsc@gmail.com>
2018-01-13 23:49:00 +0300

    Added IntroPresenter in intro module Added scroll to Intro screen

---
+++ golos-ios/Modules/Intro/IntroPresenter.swift
@@ -0,0 +1,22 @@
+//
+//  IntroPresenter.swift
+//  golos-ios
+//
+//  Created by Grigory Serebryanyy on 13/01/2018.
+//  Copyright © 2018 golos. All rights reserved.
+//
+
+import Foundation
+
+class IntroPresenter: NSObject {
+    let scrollStrings: [String]
+    
+    override init() {
+        scrollStrings = [
+            "Блог Платформа\nследующего поколения",
+            "Блог Платформа\nследующего поколения",
+            "Блог Платформа\nследующего поколения",
+            "Блог Платформа\nследующего поколения"
+        ]
+    }
+}
---
+++ golos-ios/Modules/Intro/IntroViewController/IntroViewController.swift
@@ -0,0 +1,106 @@
+//
+//  IntroViewController.swift
+//  golos-ios
+//
+//  Created by Grigory Serebryanyy on 13/01/2018.
+//  Copyright © 2018 golos. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    //MARK: Outlets
+    @IBOutlet weak var enterButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var moreInfoButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    //MARK: UI
+    var scrollLabels = [UILabel]()
+    
+    //MARK: Module
+    let presenter = IntroPresenter()
+    
+    
+    //MARK: Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    
+    //MARK: SetupUI
+    private func setupUI() {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        enterButton.setBlueButtonRoundEdges()
+        registerButton.setBorderButtonRoundEdges()
+        moreInfoButton.setTitleColor(UIColor.Project.buttonTextGray, for: .normal)
+        
+        pageControl.numberOfPages = presenter.scrollStrings.count
+        
+        setupScrollView()
+    }
+    
+    private func setupScrollView() {
+        scrollView.delegate = self
+        let strings = presenter.scrollStrings
+        for string in strings {
+            let label = UILabel()
+            label.font = Fonts.shared.regular(with: 16.0)
+            label.textColor = UIColor.Project.textBlack
+            label.text = string
+            label.numberOfLines = 0
+            label.textAlignment = .center
+            scrollView.addSubview(label)
+            scrollLabels.append(label)
+        }
+    }
+    
+    
+    //MARK: Layout
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        for (index, label) in scrollLabels.enumerated() {
+            let x = scrollView.bounds.size.width * CGFloat(index)
+            var scrollViewBounds = scrollView.bounds
+            scrollViewBounds.origin.x = x
+            label.frame = scrollViewBounds
+        }
+        scrollView.contentSize = CGSize(width: scrollView.bounds.size.width * CGFloat(scrollLabels.count), height: scrollView.bounds.size.height)
+    }
+    
+    
+    //MARK: Actions
+    @IBAction func enterButtonPressed(_ sender: Any) {
+        Utils.inDevelopmentAlert()
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: Any) {
+        Utils.inDevelopmentAlert()
+    }
+    
+    @IBAction func moreInfoButtonPressed(_ sender: Any) {
+        guard let moreUrl = URL.init(string: Constants.Urls.moreInfoAbout) else {
+            Utils.showAlert(title: "Wrong more info url", message: "Developer error!")
+            return
+        }
+        
+        UIApplication.shared.open(moreUrl, options: [:], completionHandler: nil)
+    }
+}
+
+
+//MARK: UIScrollViewDelegate
+extension IntroViewController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let scrollViewWidth = scrollView.bounds.size.width
+        let pageNumber = scrollView.contentOffset.x / scrollViewWidth
+        pageControl.currentPage = Int(pageNumber)
+        print(scrollView.contentOffset.x / scrollViewWidth)
+        print(scrollView.contentOffset)
+    }
+}
