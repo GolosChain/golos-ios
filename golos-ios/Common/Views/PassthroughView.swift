86675ca8c9a688504c64d2a0189f76ab9bd8e99e
SerhiiM <msm72@mail.ru>
2018-06-28 12:41:37 +0300

    Modify & localize labels in ProfileInfoView #16

--- golos-ios/Common/Views/PassthroughView.swift
+++ golos-ios/Common/Views/PassthroughView.swift
@@ -11,6 +11,7 @@ import UIKit
 class PassthroughView: UIView {
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let view = super.hitTest(point, with: event)
+        
         return view != self && view is UIControl ? view : nil
     }
 }
--- golos-ios/Scenes/UserProfile/Views/ProfileInfoView/ProfileInfoView.swift
+++ golos-ios/Scenes/UserProfile/Views/ProfileInfoView/ProfileInfoView.swift
@@ -7,20 +7,18 @@
 //
 
 import UIKit
+import GoloSwift
+import SwiftTheme
 
 class ProfileInfoView: PassthroughView {
-    
-    // MARK: Setter properties
+    // MARK: - Properties
     var information: String? {
         get {
             return informationLabel.text
         }
         set {
             informationLabel.text = newValue
-//            guard let height = information?.height(with: informationLabel.font, width: bounds.size.width) else {
-//                return
-//            }
-//            infoLabelHeightConstraint.constant = height
+
             setNeedsLayout()
             layoutIfNeeded()
         }
@@ -30,6 +28,7 @@ class ProfileInfoView: PassthroughView {
         get {
             return postsAmountLabel.text
         }
+        
         set {
             postsAmountLabel.text = newValue
         }
@@ -39,6 +38,7 @@ class ProfileInfoView: PassthroughView {
         get {
             return subscribersAmountLabel.text
         }
+        
         set {
             subscribersAmountLabel.text = newValue
         }
@@ -48,50 +48,56 @@ class ProfileInfoView: PassthroughView {
         get {
             return subscribtionsAmountLabel.text
         }
+        
         set {
             subscribtionsAmountLabel.text = newValue
         }
     }
     
-    // MARK: Outlets properties
+    
+    // MARK: - IBOutlets
     @IBOutlet private weak var informationLabel: UILabel!
     @IBOutlet private weak var postsAmountLabel: UILabel!
     @IBOutlet private weak var subscribersAmountLabel: UILabel!
     @IBOutlet private weak var subscribtionsAmountLabel: UILabel!
-        
-//    @IBOutlet weak var infoLabelHeightConstraint: NSLayoutConstraint!
     
+    @IBOutlet var labelsCollection: [UILabel]! {
+        didSet {
+            _ = labelsCollection.map({
+                $0.text?.localize()
+                $0.font                 =   UIFont(name: "SFUIDisplay-Regular", size: 12.0 * widthRatio)
+                $0.theme_textColor      =   darkGrayWhiteColorPickers
+                $0.textAlignment        =   .left
+                $0.numberOfLines        =   1
+            })
+        }
+    }
     
-    
-    // MARK: Init
+    // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+       
         commonInit()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        
         commonInit()
     }
     
+    
+    // MARK: - Custom Functions
     private func commonInit() {
         let nib = UINib(nibName: String(describing: ProfileInfoView.self), bundle: nil)
         let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        
         addSubview(view)
         
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        view.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        view.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        view.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-        
-        setupUI()
+        view.translatesAutoresizingMaskIntoConstraints                  =   false
+        view.topAnchor.constraint(equalTo: topAnchor).isActive          =   true
+        view.rightAnchor.constraint(equalTo: rightAnchor).isActive      =   true
+        view.leftAnchor.constraint(equalTo: leftAnchor).isActive        =   true
+        view.bottomAnchor.constraint(equalTo: bottomAnchor).isActive    =   true
     }
-    
-    
-    // MARK: Setup UI
-    private func setupUI() {
-        
-    }
-    
 }
