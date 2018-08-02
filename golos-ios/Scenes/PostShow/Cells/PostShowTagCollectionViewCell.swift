7f63c3f60122cf22fd02b699969195ad9a5b3f10
SerhiiM <msm72@mail.ru>
2018-08-02 20:50:21 +0300

    Create custom post tag cell #32.

---
+++ golos-ios/Scenes/PostShow/Cells/PostShowTagCollectionViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  PostShowTagCollectionViewCell.swift
+//  Golos
+//
+//  Created by msm72 on 02.08.2018.
+//  Copyright © 2018 golos. All rights reserved.
+//
+
+import UIKit
+import GoloSwift
+
+class PostShowTagCollectionViewCell: UICollectionViewCell {
+    // MARK: - Properties
+    var completionButtonTapped: (() -> Void)?
+
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var tagButton: UIButton! {
+        didSet {
+            tagButton.tune(withTitle:       "",
+                           hexColors:       [darkGrayWhiteColorPickers, lightGrayWhiteColorPickers, lightGrayWhiteColorPickers, lightGrayWhiteColorPickers],
+                           font:            UIFont(name: "SFUIDisplay-Medium", size: 12.0 * widthRatio),
+                           alignment:       .center)
+            
+            tagButton.setBorder(color: UIColor(hexString: "#e0e0e0").cgColor, cornerRadius: tagButton.bounds.height * heightRatio / 2)
+        }
+    }
+    
+
+    // MARK: - Class Initialization
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    deinit {
+        Logger.log(message: "Success", event: .severe)
+    }
+    
+    
+    // MARK: - Actions
+    @IBAction func tagButtonTapped(_ sender: UIButton) {
+        sender.setBorder(color: UIColor(hexString: "#e0e0e0").cgColor, cornerRadius: sender.bounds.height * heightRatio / 2)
+
+        self.completionButtonTapped!()
+    }
+    
+    @IBAction func tagButtonTappedDown(_ sender: UIButton) {
+        sender.setBorder(color: UIColor(hexString: "#dbdbdb").cgColor, cornerRadius: sender.bounds.height * heightRatio / 2)
+    }
+}
+
+
+// MARK: - ConfigureCell
+extension PostShowTagCollectionViewCell: ConfigureCell {
+    func setup(withItem item: Any?, andIndexPath indexPath: IndexPath) {
+        if let tag = item as? Tag {
+            self.tagButton.setTitle(tag.title!, for: .normal)
+        }
+    }
+}
